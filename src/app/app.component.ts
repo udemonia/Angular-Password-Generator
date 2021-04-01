9503a0e11476b943817ad35d4d8cfbda8c833d7a
udemonia <brandon.l.lambert@icloud.com>
2021-04-01 07:43:20 -0500

    finished project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'passwordGenerator';
+  length = 0;
+  password = '';
+  includeLetters = false
+  includeNumbers = false
+  includeSymbols = false
+
+  onChangeLength(value: string) {
+    const numberValue = parseInt(value)
+    if (!isNaN(numberValue)) {
+      this.length = numberValue
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters
+  }
+  onChangeNumbers() {
+    this.includeNumbers = !this.includeNumbers
+  }
+  onChangeSymbol() {
+    this.includeSymbols = !this.includeSymbols
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890'
+    const letters = 'abcdefghijklmnopqrstuvwxyz'
+    const symbols = '!@#$%^&*'
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters
+    }
+    if (this.includeNumbers) {
+      validChars += numbers
+    }
+    if (this.includeSymbols) {
+      validChars += symbols
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length)
+      generatedPassword += validChars[index]
+    }
+
+
+    this.password = generatedPassword
+    console.log(this.includeLetters)
+    console.log(this.includeNumbers)
+    console.log(this.includeSymbols)
+    console.log(`Number Length: ${this.length}`)
+  }
 }
